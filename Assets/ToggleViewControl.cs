aa2a128f6a560db48cc9c125426024d1abb7687b
rikochet9 <darrenwjoe@gmail.com>
2024-07-08 13:07:17 -0700

    TDTK interface now controllable in first person view. Added TDTK layer tags to project settings.

--- Assets/ToggleViewControl.cs
+++ Assets/ToggleViewControl.cs
@@ -8,35 +8,60 @@ public class ToggleViewControl : MonoBehaviour
     [SerializeField] List<GameObject> overheadViewObjects;
     [Tooltip("Specify objects to be enabled if and only if in first person view.")]
     [SerializeField] List<GameObject> firstPersonViewObjects;
-
-
-    [Header("Monitoring")]
+    
     [SerializeField] bool inFirstPerson = false; //ViewEnum currentView = ViewEnum.Overhead;
+    [SerializeField] bool mouseLocked;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetViewValues();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.Tab))
             ToggleView();
+
+        if (inFirstPerson && Input.GetKeyDown(KeyCode.X))
+            ToggleMouse();
     }
 
     void ToggleView()
     {
         inFirstPerson = !inFirstPerson;
+        SetViewValues();
+    }
 
+    void SetViewValues()
+    {
         ToggleGameObjects(overheadViewObjects, !inFirstPerson);
         ToggleGameObjects(firstPersonViewObjects, inFirstPerson);
 
+        if (inFirstPerson)
+            SetMouse(mouseLocked);
+        else
+            SetMouse(false); // mouse is always on in Overview mode.
+    }
 
-        if (!inFirstPerson)
+    void ToggleGameObjects(List<GameObject> list, bool activateThem)
+    {
+        foreach (GameObject gameObject in list)
+            gameObject.SetActive(activateThem);
+    }
+
+    void ToggleMouse()
+    {
+        mouseLocked = !mouseLocked;
+        SetMouse(mouseLocked);
+    }
+
+    void SetMouse(bool toLocked)
+    {
+        if (!toLocked)
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
@@ -47,12 +72,4 @@ public class ToggleViewControl : MonoBehaviour
             Cursor.visible = false;
         }
     }
-
-    void ToggleGameObjects(List<GameObject> list, bool activateThem)
-    {
-        foreach (GameObject gameObject in list)
-            gameObject.SetActive(activateThem);
-    }
-
-
 }
