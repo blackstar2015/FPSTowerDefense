4efaff4e84732f37956093643fb7e7075301a28c
Pradeep Pillai <ppillai05@gmail.com>
2024-07-18 18:09:48 -0700

    Fixed FMOD errors in menu scene. also completed FMOD setup

--- Assets/Scripts/UIScripts/AudioParameterSetting.cs
+++ Assets/Scripts/UIScripts/AudioParameterSetting.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using FMOD;
+using FMODUnity;
 using Debug = UnityEngine.Debug;
 
 #if FMOD
@@ -14,18 +16,18 @@ using FMODUnity;
 public class AudioParameterSetting : FloatSetting
 {
     [Header("Mixer")]
-#if FMOD
+//#if FMOD
     [SerializeField, ParamRef] private string _fmodParameter = "";      //FMOD parameter selector in inspector
-#else
-    [SerializeField] private AudioMixer _audioMixer;
-    [SerializeField] private string _parameterName = "Parameter Name";
-#endif
+//#else
+    //[SerializeField] private AudioMixer _audioMixer;
+    //[SerializeField] private string _parameterName = "Parameter Name";
+//#endif
 
     public override void SetValue(float newValue)
     {
         base.SetValue(newValue);
 
-#if FMOD
+//#if FMOD
         if (!string.IsNullOrEmpty(_fmodParameter))
         {
             RESULT result = RuntimeManager.StudioSystem.setParameterByName(_fmodParameter, newValue);
@@ -34,8 +36,8 @@ public class AudioParameterSetting : FloatSetting
                 Debug.LogError(string.Format("[FMOD] StudioGlobalParameterTrigger failed to set parameter {0} : result = {1}", _fmodParameter, result));
             }
         }
-#else
-        _audioMixer.SetFloat(_parameterName, newValue);
-#endif
+//#else
+//        _audioMixer.SetFloat(_parameterName, newValue);
+//#endif
     }
 }
