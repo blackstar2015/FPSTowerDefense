f9e45ee497092e1168f1453c1bfd244026ff83e4
Pradeep Pillai <ppillai@vfs.com>
2024-07-26 18:35:21 -0700

    KAN-105 Implemented cooldown on slingshot

--- Assets/Scripts/Weapons/WeaponRangedProjectile.cs
+++ Assets/Scripts/Weapons/WeaponRangedProjectile.cs
@@ -13,22 +13,19 @@ public class WeaponRangedProjectile : Weapon
     [field: SerializeField, BoxGroup("Weapon")] private float _chargingFactor = 2f;    
     [field: SerializeField, BoxGroup("Weapon")] public float Cooldown { get; protected set; } = 0.5f;
     [field: SerializeField, BoxGroup("SFX")] public EventReference ChargeAttackSFX { get; protected set; }
+
     [SerializeField, BoxGroup("Camera")] private float _minFOV = 30f;
     [SerializeField, BoxGroup("Camera")] private float _maxFOV = 60f;
     [SerializeField, BoxGroup("Camera")] private CinemachineVirtualCamera _camera;
+
     private float _minCharge = 0f;
     private float _maxCharge = 100f;
     public float CurrentCharge { get; set; } = 0f;
     public bool IsCharging { get; set; } = false;
-
-
     public float ChargePercentage => CurrentCharge/_maxCharge;
-    
-    private float _lastAttackTime;
 
     public void ChargeAttack()
     {
-        //if(!enabled) return;
         StartCoroutine(ChargeWeaponRoutine());
     }
 
@@ -40,17 +37,10 @@ public class WeaponRangedProjectile : Weapon
         RaycastHit hit;
         Vector3 targetPoint;
 
-        if (Physics.Raycast(ray, out hit))
-        {
-            targetPoint = hit.point;
-        }
-        else
-        {
-            targetPoint = ray.GetPoint(Range);
-        }
+        if (Physics.Raycast(ray, out hit))    targetPoint = hit.point;
+        else targetPoint = ray.GetPoint(Range);
 
         Vector3 direction = (targetPoint - _muzzle.position).normalized;
-
         rb.velocity = direction * _projectileSpeed + direction * _projectileSpeed * ChargePercentage;
     }
 
@@ -74,10 +64,7 @@ public class WeaponRangedProjectile : Weapon
             _camera.m_Lens.FieldOfView = Mathf.Lerp(_maxFOV, _minFOV, CurrentCharge / 100);
             yield return null;
         }
-
     }
-
-    
 
     protected override void Update()
     {
